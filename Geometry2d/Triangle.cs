7c86099f45265f2f898b82914ec03135fdbf201f
Riku Erkkila <riku.erkkila@gmail.com>
2024-11-24 19:20:58 +0100

    new voronoi

--- Geometry2d/Triangle.cs
+++ Geometry2d/Triangle.cs
@@ -11,6 +11,8 @@ namespace RikusGameDevToolbox.Geometry2d
 
         public Vector2[] Vertices => new[] { Vertex1, Vertex2, Vertex3 };
         public Edge[] Edges => new[] { new Edge(Vertex1, Vertex2), new Edge(Vertex2, Vertex3), new Edge(Vertex3, Vertex1) };
+        public Vector2 Centroid => (Vertex1 + Vertex2 + Vertex3) / 3f;
+        public Vector2 Circumcenter => CalculateCircumcenter();
 
         public Triangle(Vector2 v1, Vector2 v2, Vector2 v3)
         {
@@ -20,8 +22,12 @@ namespace RikusGameDevToolbox.Geometry2d
             }
 
             (Vertex1, Vertex2, Vertex3) = (v1, v2, v3);
+
         }
-   
+
+
+
+
         public bool Equals(Triangle other)
         {
             return ( Vertex1 == other.Vertex1 || Vertex1 == other.Vertex2 || Vertex1 == other.Vertex3 ) &&
@@ -42,6 +48,22 @@ namespace RikusGameDevToolbox.Geometry2d
         public override string ToString()
         {
             return "Triangle: " + Vertex1 + ", " + Vertex2 + ", " + Vertex3;
+        }
+        
+        private Vector2 CalculateCircumcenter()
+        {
+            float x1 = Vertex1.x;
+            float x2 = Vertex2.x;
+            float x3 = Vertex3.x;
+            float y1 = Vertex1.y;
+            float y2 = Vertex2.y;
+            float y3 = Vertex3.y;
+
+            float D = 2 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+            float Ux = ((x1 * x1 + y1 * y1) * (y2 - y3) + (x2 * x2 + y2 * y2) * (y3 - y1) + (x3 * x3 + y3 * y3) * (y1 - y2)) / D;
+            float Uy = ((x1 * x1 + y1 * y1) * (x3 - x2) + (x2 * x2 + y2 * y2) * (x1 - x3) + (x3 * x3 + y3 * y3) * (x2 - x1)) / D;
+
+            return new Vector2(Ux, Uy);
         }
     }
 }
